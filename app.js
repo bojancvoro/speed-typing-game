4c95cac99e0eab32f9eb868d1e8ca3c9e07b9b64
bojancvoro <bojan.cvoro@gmail.com>
2018-08-24 15:00:06 +0200

    add games status check

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+let score = 0;
+let timeLeft = 5;
+
 // Model
 // data and app logic
 
-let score = 0;
 
 const Model = (function () {
     const words = ["compensation", "consciousness", "shift", "knowledge", "pierce",
@@ -12,8 +14,6 @@ const Model = (function () {
         return words[randomIndex];
     }
 
-    // add handle wrong answer, handle time expired
-
     function handleUpdateScore(word, userInput) {
         if (userInput === word) {
             score++;
@@ -22,14 +22,19 @@ const Model = (function () {
         }
     }
 
-    function handleTrackGameStatus() {
+    function handleTrackGameStatus(handleDisplayGameActive) {
+        const intervalId = setInterval(() => {
+            handleDisplayGameActive(timeLeft > 0);
+        }, 200);
 
+        return intervalId;
     }
 
     return {
         score,
         handlePickWord,
-        handleUpdateScore
+        handleUpdateScore,
+        handleTrackGameStatus
     }
 })();
 
@@ -54,19 +59,35 @@ const View = (function () {
     function handleTakeInput(e) {
         userInput = e.target.value;
         e.target.value = "";
-        return userInput
+        return userInput;
     }
+
 
     function handleDisplayTimeLeft() {
         const timeLeftElement = document.getElementById("time-left");
-        let timeLeft = 5;
+        timeLeft = 5;
+
         const inervalId = setInterval(() => {
             if (timeLeft > 0) {
                 timeLeft--;
             }
             timeLeftElement.innerHTML = timeLeft;
         }, 1000);
+
         return inervalId;
+    }
+
+    function handleDisplayGameActive(gameActive) {
+        const gameOverElement = document.getElementById("game-over");
+        const startAgainBtn = document.getElementById("btn");
+
+        if (gameActive) {
+            gameOverElement.innerHTML = "";
+            startAgainBtn.disabled = true;
+        } else {
+            gameOverElement.innerHTML = "Game over";
+            startAgainBtn.disabled = false;
+        }
     }
 
     return {
@@ -74,6 +95,7 @@ const View = (function () {
         handleDisplayScore,
         handleTakeInput,
         handleDisplayTimeLeft,
+        handleDisplayGameActive,
         userInput
     }
 
@@ -86,27 +108,37 @@ const View = (function () {
 const Controller = (function (Model, View) {
 
     const inputElement = document.getElementById("input");
-    
+    const startAgainBtn = document.getElementById("btn");
+
+
     window.addEventListener("load", init);
     inputElement.addEventListener("change", handleTakeInput);
+    startAgainBtn.addEventListener("click", init);
 
     let word;
-    let intId;
+    let timeLeftInterval;
+    let gameStatusInterval;
 
     function init() {
+        clearInterval(timeLeftInterval);
+        clearInterval(gameStatusInterval);
+        timeLeftInterval = View.handleDisplayTimeLeft();
+        gameStatusInterval = Model.handleTrackGameStatus(View.handleDisplayGameActive);
         word = Model.handlePickWord();
-        Model.handleUpdateScore(word);
         View.handleDisplayWord(word);
+        Model.handleUpdateScore(word);
+        View.handleDisplayScore(0);
+        View.handleDisplayGameActive(true);
     }
 
     function handleTakeInput(e) {
-        clearInterval(intId);
+        clearInterval(timeLeftInterval);
         const userInput = View.handleTakeInput(e);
         Model.handleUpdateScore(word, userInput);
         View.handleDisplayScore(score);
         word = Model.handlePickWord();
         View.handleDisplayWord(word);
-        intId = View.handleDisplayTimeLeft();
+        timeLeftInterval = View.handleDisplayTimeLeft();
     }
 
 })(Model, View);
